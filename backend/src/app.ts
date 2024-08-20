f5d8d6d9b7d38fa1fe3070f077f5f7a6080b26c7
Salvatore Santoro <santorosalvatoreunina@gmail.com>
2024-08-20 18:50:33 +0200

    added posting hotel functionality

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import myHotelRoutes from "./routes/my-hotels";
 import { errorHandler } from "./middlewares/errorHandler";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -12,19 +20,20 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL as string,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
-
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-
 
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("api/my-hotels", myHotelRoutes);
 
 // Error Handling
 app.use(errorHandler);
---
+++ backend/src/controllers/my-hotelsController.ts
@@ -0,0 +1,30 @@
+import asyncHandler from "../utils/asyncHandler";
+import { Request, Response } from "express";
+import cloudinary from "cloudinary";
+import Hotel, { HotelType } from "../models/hotel";
+import { validationResult } from "express-validator";
+import { ResponseError } from "../middlewares/errorHandler";
+
+export const createHotel = asyncHandler(async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) throw new ResponseError(errors.array(), 400);
+  
+  const imageFiles = req.files as Express.Multer.File[];
+  const newHotel: HotelType = req.body;
+
+  const uploadPromises = imageFiles.map(async (image) => {
+    const b64 = Buffer.from(image.buffer).toString("base64");
+    let dataURI = "data:" + image.mimetype + ";base64," + b64;
+    const res = await cloudinary.v2.uploader.upload(dataURI);
+    return res.url;
+  });
+
+  const imageUrls = await Promise.all(uploadPromises);
+  newHotel.imageUrls = imageUrls;
+  newHotel.lastUpdated = new Date();
+  newHotel.userId = req.userId;
+
+  const hotel = new Hotel(newHotel);
+  await hotel.save();
+  res.status(201).send(hotel);
+});
