98775c07d98e3d7cd097cab7fc3fdb1c226cfae3
Salvatore Santoro <santorosalvatoreunina@gmail.com>
2024-10-12 19:22:24 +0200

    Added other Search functionality

--- backend/src/controllers/hotelsController.ts
+++ backend/src/controllers/hotelsController.ts
@@ -2,13 +2,33 @@ import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/types";
 import asyncHandler from "../utils/asyncHandler";
 import { Request, Response } from "express";
+import constructSearchQuery from "../utils/constructSearchQuery";
 
 export const searchHotel = asyncHandler(async (req: Request, res: Response) => {
+  const query = constructSearchQuery(req.query);
+  //console.log(req.query)
+
+  let sortOptions = {};
+  switch (req.query.sortOption) {
+    case "starRating":
+      sortOptions = { starRating: -1 };
+      break;
+    case "pricePerNightAsc":
+      sortOptions = { pricePerNight: 1 };
+      break;
+    case "pricePerNightDesc":
+      sortOptions = { pricePerNight: -1 };
+      break;
+  }
+
   const pageSize = 5;
   const pageNumber = parseInt(req.query.page ? req.query.page.toString() : "1");
   const skip = (pageNumber - 1) * pageSize;
-  const hotels = await Hotel.find().skip(skip).limit(pageSize);
-  const total = await Hotel.countDocuments();
+  const hotels = await Hotel.find(query)
+    .sort(sortOptions)
+    .skip(skip)
+    .limit(pageSize);
+  const total = await Hotel.countDocuments(query);
   const response: HotelSearchResponse = {
     data: hotels,
     pagination: {
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,10 +1,75 @@
+import { useQuery } from "@tanstack/react-query";
 import { useSearchContext } from "../contexts/SearchContext";
+import * as apiClient from "../api-client";
+import { useState } from "react";
+import SearchResultCard from "../components/SearchResultCard";
+import Pagination from "../components/Pagination";
+import StarRatingFilter from "../components/StarRatingFilter";
 
 const Search = () => {
-    const search = useSearchContext();
-    console.log(search);
+  const search = useSearchContext();
+  const [page, setPage] = useState<number>(1);
+  const [selectedStars, setSelectedStars] = useState<string[]>([]);
 
-    return(<>Search Page</>)
+  const handleStarsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const starRating = event.target.value;
+    setSelectedStars((prevStars) =>
+      event.target.checked
+        ? [...prevStars, starRating]
+        : prevStars.filter((star) => star !== starRating)
+    );
+  };
+
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString(),
+    stars: selectedStars
+  };
+
+  const { data: hotelData } = useQuery({
+    queryKey: ["searchHotels", searchParams],
+    queryFn: () => apiClient.searchHotels(searchParams),
+  });
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter by:
+          </h3>
+          <StarRatingFilter
+            selectedStars={selectedStars}
+            onChange={handleStarsChange}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? ` in ${search.destination}` : ""}
+          </span>
+          {/*TODO sort options*/}
+        </div>
+        {hotelData?.data.map((hotel) => (
+          <SearchResultCard hotel={hotel} />
+        ))}
+        <div>
+          <Pagination
+            page={hotelData?.pagination.page || 1}
+            pages={hotelData?.pagination.pages || 1}
+            onPageChange={(page) => setPage(page)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
